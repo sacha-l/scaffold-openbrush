40a7424e3e35a9d6a803ab608fa16ab1bfd2d893
Sacha Lansky <sacha@parity.io>
2023-04-06 18:38:37 +0200

    staking impls: step 2

--- contracts/steakoin/lib.rs
+++ contracts/steakoin/lib.rs
@@ -1,35 +1,38 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 #![feature(min_specialization)]
-        
+
 #[openbrush::contract]
-pub mod steakoin {
-    
-    // Imports from openbrush
+pub mod my_psp22 {
+
+    // imports from openbrush
     use openbrush::{
-        contracts::psp22::{
-            Data as PSP22Data,
-            *,
-        },
+        contracts::psp22::*,
         traits::Storage,
     };
 
-    // Defines storage for your contract
+    // The storage struct of our Staking contract
     #[ink(storage)]
     #[derive(Default, Storage)]
-    pub struct Steakoin {
-    	#[storage_field]
-		psp22: psp22::Data,
+    pub struct Staking {
+        /// Holds all PSP22 data as per [the PSP22 standard](https://github.com/w3f/PSPs/blob/master/PSPs/psp-22.md)
+        #[storage_field]
+        psp22: psp22::Data,
     }
-    
+
     // Section contains default implementation without any modifications
-	impl PSP22 for Steakoin {}
-     
-    impl Steakoin {
+    impl PSP22 for Staking {}
+
+    impl Staking {
         #[ink(constructor)]
-        pub fn new(initial_supply: Balance) -> Self {
+        /// Mint a fixed supply of 42_000_000 Staking tokens.
+        ///
+        /// Tokens are issued to the account instantiating this contract.
+        pub fn new() -> Self {
             let mut _instance = Self::default();
-			_instance._mint_to(_instance.env().caller(), initial_supply).expect("Should mint"); 
-			_instance
+            _instance
+                ._mint_to(_instance.env().caller(), 42_000_000 * 10u128.pow(18))
+                .expect("Should mint");
+            _instance
         }
     }
 }
--- src/impls/staking.rs
+++ src/impls/staking.rs
@@ -1,9 +1,19 @@
 pub use crate::traits::staking::*;
 use openbrush::{
+    storage::Mapping,
     traits::{
-        Storage,
+        Storage, Timestamp
     },
 };
+
+pub const STORAGE_KEY: u32 = openbrush::storage_unique_key!(Data);
+
+#[openbrush::upgradeable_storage(STORAGE_KEY)]
+#[derive(Default, Debug)]
+pub struct Data {
+    pub staked: Mapping<AccountId, (Balance, Timestamp)>,
+    pub _reserved: Option<()>,
+}
 
 impl<T> Staking for T
 where
@@ -11,5 +21,89 @@ where
     T: PSP22,
     T: psp22::Internal,
 {
-// Staking trait impl block goes here
+    fn stake(&mut self, amount: Balance) -> Result<(), StakingErr> {
+        
+        // get the AccountId of this caller
+        let caller = Self::env().caller();
+        
+        // get the staking data which will return a &Option<(u128, u64)
+        let staked = &self.data::<Data>().staked.get(&caller);
+
+        // users must enter an amount greater than zero
+        if amount == 0 {
+            return Err(StakingErr::AmountMustBeAboveZero)
+        }
+
+        // if the caller has some tokens already staked, accumulate the amount
+        if let Some(staking_data) = staked {
+            // accumulate stake using Internal trait
+            self._accummulate(&staking_data)?;
+        } else {
+            // otherwise, insert the amount to the staking data 
+            let _ = &self
+                .data::<Data>()
+                .staked
+                .insert(&caller, &(amount, Self::env().block_timestamp()));
+        }
+        
+        // transfer the amount to the contract's account ID
+        self.transfer_from(caller, Self::env().account_id(), amount, Vec::default())?;
+
+        Ok(())
+    }
+
+    fn unstake(&mut self, amount: Balance) -> Result<(), StakingErr> {
+        
+        // get the AccountId of this caller
+        let caller = Self::env().caller();
+        
+        // get the staking data which will return a &Option<(u128, u64)
+        let staked = &self.data::<Data>().staked.get(&caller);
+
+        // users must enter an amount greater than zero
+        if amount == 0 {
+            return Err(StakingErr::AmountMustBeAboveZero)
+        }
+
+        if let Some(staking_data) = staked {
+            
+            // accumulate stake using Internal trait
+            let new_amount = self._accummulate(&staking_data)?;
+            
+            // if user input is equal to or more than their current stake, withdraw max
+            if amount >= new_amount {
+                self.data().staked.remove(&caller);
+
+                // transfer the amount wanted to be unstaked back to the caller
+                self.transfer_from(Self::env().account_id(), caller, new_amount, Vec::default())?;
+            } else {
+
+                // update the staked amount 
+                self.data()
+                    .staked
+                    .insert(&caller, &(new_amount - amount, Self::env().block_timestamp()));
+                
+                // transfer the amount wanted to be unstaked back to the caller
+                self.transfer_from(Self::env().account_id(), caller, amount, Vec::default())?;
+            }
+        } else {
+            // no stake
+            return Err(StakingErr::NothingToWithdraw)
+        }
+
+        Ok(())
+    }
+
+    fn voting_power(&self, account: AccountId) -> u128 {
+        
+        // get the amount the account ID has staked 
+        let staked = &self.data::<Data>().staked.get(&account);
+
+        // use Internal trait to get voting power
+        if let Some(staking_data) = staked {
+            return self._get_accumulated_amount(&staking_data)
+        } else {
+            0
+        }
+    }
 }
